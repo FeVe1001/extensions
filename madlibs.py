d12cb03c04d9d7fb7f04f4d545fb0be0e871ae5f
Felix <96533009+FeVe1001@users.noreply.github.com>
2021-12-25 17:25:11 +0000

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,18 @@
+# this is my second code
+
+# some ways to do this
+# print("subscribe to " + youtuber)
+# print("subscribe to {}".format(youtuber))
+# print(f"subscribe to {youtuber}")
+
+adj= input("Adjectiv: ")
+verb1 = input("verb: ")
+verb2 = input("verb: ")
+famous_person = input ("famouse_person: ")
+
+
+madlip = f"computer programming is so {adj}! it makes me so excited all the time because \
+I  love to {verb1}. stay hydrated and {verb2} like you are {famous_person}!"
+
+
+print(madlip) 
